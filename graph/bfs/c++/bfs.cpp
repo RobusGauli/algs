2410e40def61c3df7a8fb23604231915720842a3
Robus Gauli <robusgauli@gmail.com>
2018-10-22 23:55:22 +0545

    c++ node structure

--- graph/bfs/c++/bfs.cpp
+++ graph/bfs/c++/bfs.cpp
@@ -1,9 +1,46 @@
 #include <iostream>
+#include <vector>
 
+using namespace std;
+using namespace std;
+
+// class representing node
+namespace bfs {
+  class Node 
+  {
+    public:
+      int value;
+      Node* parent;
+      std::string nodeType;
+      std::vector<Node*> edges;
+      bool visited;
+
+   public:
+    Node(int value, std::string nodeType): value(value), nodeType(nodeType) {visited = false;};
+
+    void addEdge(Node* node)
+    {
+      this -> edges.push_back(node);
+    }
+
+    bool equals(Node* node)
+    {
+      return node -> value  == this -> value;
+    }
+
+    void connect(Node* node)
+    {
+      this -> edges.push_back(node);
+      node -> edges.push_back(this);
+    }
+
+  };
+  
+}
 int main()
 {
-  std::cout << "Breadh first search implementation" << std::endl;
-
+  bfs::Node* node = new bfs::Node(3, "STRIN");
+  cout << node -> value << endl;
   return 0;
 }
 
