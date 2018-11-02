49f02834c2346fa4dfe5c8834dba239052159602
Robus Gauli <robusgauli@gmail.com>
2018-11-02 09:03:08 +0545

    golang implementation of dbl

--- graph/doubly-linked-list/golang/main.go
+++ graph/doubly-linked-list/golang/main.go
@@ -1,100 +1,88 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"os"
-
-	"golang.org/x/net/context"
-	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/google"
-	"google.golang.org/api/gmail/v1"
 )
 
-// Retrieve a token, saves the token, then returns the generated client.
-func getClient(config *oauth2.Config) *http.Client {
-	tokFile := "token.json"
-	tok, err := tokenFromFile(tokFile)
-	if err != nil {
-		tok = getTokenFromWeb(config)
-		saveToken(tokFile, tok)
-	}
-	return config.Client(context.Background(), tok)
+type Node struct {
+	value    int
+	nextNode *Node
+	prevNode *Node
 }
 
-// Request a token from the web, then returns the retrieved token.
-func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
-	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
-	fmt.Printf("Go to the following link in your browser then type the "+
-		"authorization code: \n%v\n", authURL)
-
-	var authCode string
-	if _, err := fmt.Scan(&authCode); err != nil {
-		log.Fatalf("Unable to read authorization code: %v", err)
-	}
-
-	tok, err := config.Exchange(context.TODO(), authCode)
-	if err != nil {
-		log.Fatalf("Unable to retrieve token from web: %v", err)
-	}
-	return tok
+func (node *Node) String() string {
+	return fmt.Sprintf("Node(%d)", node.value)
 }
 
-// Retrieves a token from a local file.
-func tokenFromFile(file string) (*oauth2.Token, error) {
-	f, err := os.Open(file)
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-	tok := &oauth2.Token{}
-	err = json.NewDecoder(f).Decode(tok)
-	return tok, err
+func Create(value int) *Node {
+	return &Node{value: value}
 }
 
-// Saves a token to a file path.
-func saveToken(path string, token *oauth2.Token) {
-	fmt.Printf("Saving credential file to: %s\n", path)
-	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
-	if err != nil {
-		log.Fatalf("Unable to cache oauth token: %v", err)
+// method on a node struct
+func (node *Node) setLeft(n *Node) {
+	node.prevNode = n
+}
+
+// method on node struct
+func (node *Node) setRight(n *Node) {
+	node.nextNode = n
+}
+
+func fromList(alist []int) (*Node, *Node) {
+	// this return first and the last node
+	var firstNode *Node
+	var currentNode *Node
+	for _, val := range alist {
+		// create a node out of it
+		n := Create(val)
+		if firstNode == nil {
+			firstNode = n
+		}
+		if currentNode != nil {
+			currentNode.setRight(n)
+			n.setLeft(currentNode)
+		}
+		currentNode = n
+
 	}
-	defer f.Close()
-	json.NewEncoder(f).Encode(token)
+
+	return firstNode, currentNode
+}
+
+func traverseLeft(node *Node) *[]int {
+	// what we can do is
+	result := make([]int, 0)
+	for {
+		if node == nil {
+			break
+		}
+		result = append(result, node.value)
+		node = node.nextNode
+	}
+	return &result
+}
+
+func traverseRight(node *Node) *[]int {
+	result := make([]int, 0)
+	for {
+		if node == nil {
+			break
+		}
+		result = append(result, node.value)
+		node = node.prevNode
+	}
+	return &result
 }
 
 func main() {
-	b, err := ioutil.ReadFile("credentials.json")
-	if err != nil {
-		log.Fatalf("Unable to read client secret file: %v", err)
-	}
+	// create a list
+	alist := []int{1, 2, 3, 4, 5}
+	firstNode, lastNode := fromList(alist)
+	// print from left to right
+	leftList := traverseLeft(firstNode)
+	fmt.Println(leftList)
 
-	// If modifying these scopes, delete your previously saved token.json.
-	config, err := google.ConfigFromJSON(b, gmail.GmailReadonlyScope)
-	if err != nil {
-		log.Fatalf("Unable to parse client secret file to config: %v", err)
-	}
-	client := getClient(config)
-
-	srv, err := gmail.New(client)
-	if err != nil {
-		log.Fatalf("Unable to retrieve Gmail client: %v", err)
-	}
-
-	user := "me"
-	r, err := srv.Users.Labels.List(user).Do()
-	if err != nil {
-		log.Fatalf("Unable to retrieve labels: %v", err)
-	}
-	if len(r.Labels) == 0 {
-		fmt.Println("No labels found.")
-		return
-	}
-	fmt.Println("Labels:")
-	for _, l := range r.Labels {
-		fmt.Printf("- %s\n", l.Name)
-	}
+	// print from right to left
+	rightList := traverseRight(lastNode)
+	fmt.Println(rightList)
 }
